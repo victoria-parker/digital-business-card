580def3675f7293965ae9a817bbcef350728ab7d
victoria-parker <victoriaparkerb@gmail.com>
2022-01-09 02:05:35 +0000

    interests modified

--- src/components/Interests.js
+++ src/components/Interests.js
@@ -4,7 +4,7 @@ export default function Interests(){
     return (
         <div className="interests">
             <h2>Interests</h2>
-            <p>Studying expert. Classical Music lover. Neuroscience fanatic. Aspiring Amateur Cellist. Chemistry geek. Rom-coms buff. </p>
+            <p>Coding geek. Studying expert. Classical Music lover. Neuroscience fanatic. Aspiring Amateur Cellist. Rom-coms buff. </p>
         </div>
     )
 }
